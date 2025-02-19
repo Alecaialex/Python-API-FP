189c8a3998dedaa3364bdabfebb1b4374d762a4d
alex <alecaialex@gmail.com>
2025-02-19 10:37:09 +0100

    Main Jugadores y Partido

---
+++ api.py
@@ -0,0 +1,46 @@
+from flask import Flask
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/jugador/<jugadorAConsultar>')
+def jugador(jugadorAConsultar):
+    if esNumero(jugadorAConsultar):
+        jugador = consultar(f"SELECT * FROM jugadores WHERE numero = {int(jugadorAConsultar)};")
+    else :
+        jugador = consultar(f"SELECT * FROM jugadores WHERE nombre = '{jugadorAConsultar}';")
+
+    return {"numero": jugador[0], "nombre": jugador[1], "edad": jugador[2], "posicion": jugador[3], "goles": jugador[4], "asistencias": jugador[5], "partidos_jugados": jugador[6]}
+
+@app.route('/ultimospartidos/<n>')
+def partido(n):
+    partido = consultar(f"SELECT * FROM partidos ORDER BY fecha DESC LIMIT 1 OFFSET {n};")
+    return {"id": partido[0], "fecha": partido[1], "rival": partido[2], "ganado": partido[3], "goles_a_favor": partido[4], "goles_en_contra": partido[5], "competicion": partido[6]}
+
+def consultar(consulta):
+    con = sqlite3.connect("test.db")
+    cur = con.cursor()
+    cur.execute(consulta)
+    resultado = cur.fetchone()
+    con.close()
+    return resultado
+
+def esNumero(n):
+    try:
+        int(n)
+        return True
+    except ValueError:
+        return False
+    
+class Jugador:
+        def __init__(self, numero, nombre, edad, posicion, goles, asistencias, partidos_jugados):
+            self.numero = numero
+            self.nombre = nombre
+            self.edad = edad
+            self.posicion = posicion
+            self.goles = goles
+            self.asistencias = asistencias
+            self.partidos_jugados = partidos_jugados
+
+if __name__ == '__main__':
+    app.run()
---
+++ bd.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+con = sqlite3.connect("test.db")
+cur = con.cursor()
+
+cur.execute("DROP TABLE IF EXISTS jugadores")
+cur.execute("CREATE TABLE jugadores (numero INTEGER PRIMARY KEY, nombre TEXT, edad INTEGER, posicion TEXT, goles INTEGER, asistencias INTEGER, partidos_jugados INTEGER)")
+cur.execute("INSERT INTO jugadores (numero, nombre, edad, posicion, goles, asistencias, partidos_jugados) VALUES (19, 'Julian Álvarez', 25, 'Delantero', 17, 4, 37)").fetchone()
+cur.execute("INSERT INTO jugadores (numero, nombre, edad, posicion, goles, asistencias, partidos_jugados) VALUES (22, 'Giuliano Simeone', 22, 'Delantero', 4, 5, 29)").fetchone()
+cur.execute("INSERT INTO jugadores (numero, nombre, edad, posicion, goles, asistencias, partidos_jugados) VALUES (5, 'Rodrigo De Paul', 30, 'Centrocampista', 3, 7, 34)").fetchone()
+cur.execute("DROP TABLE IF EXISTS partidos")
+cur.execute("CREATE TABLE partidos (id INTEGER PRIMARY KEY AUTOINCREMENT, fecha DATE, rival VARCHAR(30), ganado BOOLEAN, goles_a_favor INTEGER, goles_en_contra INTEGER, competicion TEXT)")
+cur.execute("INSERT INTO partidos (fecha, rival, ganado, goles_a_favor, goles_en_contra, competicion) VALUES ('2022-05-01', 'Real Madrid', 1, 2, 1, 'Liga')").fetchone()
+cur.execute("INSERT INTO partidos (fecha, rival, ganado, goles_a_favor, goles_en_contra, competicion) VALUES ('2022-04-08', 'Barcelona', 0, 1, 3, 'Liga')").fetchone()
+cur.execute("INSERT INTO partidos (fecha, rival, ganado, goles_a_favor, goles_en_contra, competicion) VALUES ('2022-01-15', 'Sevilla', 1, 4, 1, 'Liga')").fetchone()
+
+# Pruebas
+#print(cur.execute("SELECT * FROM jugadores WHERE numero = 19;").fetchall())
+con.commit()
+con.close()
